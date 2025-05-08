382d5380f6d01f0e7970a7086e696bbaa88c6dc0
Arif <140314705+arif-bcse@users.noreply.github.com>
2025-05-08 09:27:38 +0600

    Create Multiples.java

---
+++ Practice Problem/ch-4/Multiples.java
@@ -0,0 +1,12 @@
+
+
+public class MultiplesOfTwo {
+    public static void main(String[] args) {
+        int number = 2;
+
+        while (true) {
+            System.out.println(number);
+            number += 2;
+        }
+    }
+}
